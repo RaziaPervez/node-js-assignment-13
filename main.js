03c21049ac571742eb7a82065f79b38ead0be15b
Razia Pervez <160026294+RaziaPervez@users.noreply.github.com>
2024-03-09 10:05:24 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+//project=13
+//project=12
+//our Own Array: Think of your favorite mode of transportation, 
+//such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+var transportationMode = ["bike", "sportbike", "car", "sportcar"];
+transportationMode.forEach(function (mode) { return console.log("I would like to own a ".concat(mode)); });
---
+++ main.ts
@@ -0,0 +1,7 @@
+//project=13
+//project=12
+//our Own Array: Think of your favorite mode of transportation, 
+//such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+let transportationMode = ["bike","sportbike","car","sportcar"];
+transportationMode.forEach(mode => console.log (`I would like to own a ${mode}`));
